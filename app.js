02ff06b54268426e6b16a6bc0bb6287694551a00
mini-militant <shailesh.coolcalm007@gmail.com>
2020-04-12 09:45:07 +0530

    chn

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ if(process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'))
 
-  app.get('/*',(req,res)=>{
+  app.get('*',(req,res)=>{
 
     res.sendFile(path.join(__dirname,'build','index.html'));
   })
